1c928c9926eeee6d5ca6039c0cc9a515d4b9de8c
Developer-EJ <155644012+Developer-EJ@users.noreply.github.com>
2024-06-12 20:18:01 +0900

    고래한입피자 개선

--- lib/pages/restaurant_page.dart
+++ lib/pages/restaurant_page.dart
@@ -4,6 +4,8 @@ import '../tabs/community_tab.dart';
 import '../pages/reservation_page.dart';
 
 class RestaurantPage extends StatefulWidget {
+  const RestaurantPage({Key? key}) : super(key: key);
+
   @override
   _RestaurantPageState createState() => _RestaurantPageState();
 }
@@ -16,81 +18,91 @@ class _RestaurantPageState extends State<RestaurantPage>
   void initState() {
     super.initState();
     _tabController = TabController(length: 2, vsync: this);
-    _tabController.addListener(() {
-      setState(() {});
-    });
   }
 
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      length: 2,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Container(),
-          leading: IconButton(
-            icon:
-                Image.asset('assets/images/back_button.png'), // 뒤로가기 버튼 이미지 경로
-            onPressed: () {
-              Navigator.pop(context); // 뒤로가기 기능 구현
-            },
-          ),
-        ),
-        body: Column(
-          children: [
-            Image.asset('assets/images/top_pizza_image.png', fit: BoxFit.cover),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: const Text(
-                '고래한입피자 단국대점',
-                style: TextStyle(
-                  fontSize: 24,
-                  fontWeight: FontWeight.bold,
+    return Scaffold(
+      body: NestedScrollView(
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return <Widget>[
+            SliverAppBar(
+              expandedHeight: 300.0,
+              floating: false,
+              pinned: true,
+              flexibleSpace: FlexibleSpaceBar(
+                background: Image.asset(
+                  'assets/images/top_pizza_image.png',
+                  fit: BoxFit.cover,
+                ),
+                title: const Text('고래한입피자 단국대점'),
+              ),
+              leading: IconButton(
+                icon: Image.asset('assets/images/back_button.png'),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+            ),
+            SliverPersistentHeader(
+              delegate: _SliverAppBarDelegate(
+                TabBar(
+                  controller: _tabController,
+                  tabs: const [
+                    Tab(text: '메뉴'),
+                    Tab(text: '커뮤니티'),
+                  ],
                 ),
               ),
+              pinned: true,
             ),
-            PreferredSize(
-              preferredSize: Size.fromHeight(48.0),
-              child: Column(
-                children: [
-                  TabBar(
-                    controller: _tabController,
-                    tabs: [
-                      Tab(text: '메뉴'),
-                      const Tab(text: '커뮤니티'),
-                    ],
-                  ),
-                  Container(
-                    color: Colors.purple,
-                    height: 4.0,
-                  ),
-                ],
-              ),
-            ),
-            Expanded(
-              child: TabBarView(
-                controller: _tabController,
-                children: [
-                  MenuTab(),
-                  CommunityTab(),
-                ],
-              ),
-            ),
+          ];
+        },
+        body: TabBarView(
+          controller: _tabController,
+          children: [
+            MenuTab(),
+            CommunityTab(),
           ],
         ),
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
-        floatingActionButton: _tabController.index == 0
-            ? ElevatedButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(builder: (context) => ReservationPage()),
-                  );
-                },
-                child: const Text('예약하기'),
-              )
-            : null,
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: _tabController.index == 0
+          ? ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => ReservationPage()),
+          );
+        },
+        child: const Text('예약하기'),
+      )
+          : null,
     );
+  }
+}
+
+class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
+  _SliverAppBarDelegate(this._tabBar);
+
+  final TabBar _tabBar;
+
+  @override
+  double get minExtent => _tabBar.preferredSize.height;
+  @override
+  double get maxExtent => _tabBar.preferredSize.height;
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.white,
+      child: _tabBar,
+    );
+  }
+
+  @override
+  bool shouldRebuild(_SliverAppBarDelegate oldDelegate) {
+    return false;
   }
 }
